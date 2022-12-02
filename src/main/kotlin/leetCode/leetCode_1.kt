fa1bac2a47e6a99b64d6cacb89725ef77ec1bd7a
khurshiddev <umaribnxattob8008@gmail.com>
2022-12-02 15:26:35 +0900

    [add::] leetCode_1.kt

---
+++ src/main/kotlin/leetCode/leetCode_1.kt
@@ -0,0 +1,22 @@
+package leetCode
+
+fun main() {
+    val count = countOdds(3, 7)
+    println(count)
+
+}
+
+fun countOdds(low: Int, high: Int): Int {
+    var low2 = low
+    var high2 = high
+    if (low % 2 == 0) {
+        low2 += 1
+    }
+    if (high % 2 == 0) {
+        high2 -= 1
+    }
+    if (high2-low2 == 0) return 1
+    return ((high2-low2)/2)+1
+
+
+}
